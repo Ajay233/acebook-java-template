c563593c2692c0cbcf9d99485e4e8de3ddcca4b7
Ajay <ajaymungur@hotmail.com>
2019-01-11 17:06:35 +0000

    Added POST route and retrieved DB record

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -7,10 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
+import java.util.Optional;
+
 @Controller
 public class HomeController {
 
 	private final PostRepository postRepository;
+//	private Post post;
 
 	@Autowired
 	public HomeController(PostRepository postRepository) {
@@ -24,10 +28,27 @@ public class HomeController {
 
 	@RequestMapping(value = "/greeting")
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	public String greeting(@RequestParam(name="name", required=true, defaultValue="Everybody") String name, Model model) {
 		model.addAttribute("name", name);
 
 		return "greeting";
+	}
+
+	@RequestMapping(value = "/posts")
+	@PostMapping("/posts")
+	public String posts(Model model){
+		long id = 1;
+		Post post = new Post("TestContent");
+		model.addAttribute("post", post.getContent());
+		model.addAttribute("allPosts", postRepository.findById((long)1));
+		model.addAttribute("toString", postRepository.toString());
+		model.addAttribute("findAll", postRepository.findAll());
+		Optional<Post> post2 = postRepository.findById(id);
+		model.addAttribute("findById", post2);
+		model.addAttribute("findByIdString", post2.toString());
+		model.addAttribute("find", post2.get().getContent());
+		model.addAttribute("test", postRepository.getClass());
+		return "posts";
 	}
 
 
--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -16,12 +16,17 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String content;
 
     private Post() {}
 
     public Post(String content) {
         this.content = content;
+    }
+
+    public String getContent() {
+        return this.content;
     }
 
 }
